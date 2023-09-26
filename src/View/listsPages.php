d31b6bd685634dd8a9585c2cff975944bddd3a7a
alban-martinant-de-preneuf <alban.martinant-de-preneuf@laplateforme.io>
2023-09-26 10:01:13 +0200

    toto style done

--- src/View/listsPages.php
+++ src/View/listsPages.php
@@ -1,9 +1,9 @@
 <h1>To do list</h1>
 
 <form id="add_list_form">
-    <input type="text" id="list_title" placeholder="Titre" name="title">
+    <input type="text" id="list_title" placeholder="New list" name="title">
     <button type="submit" id="add_list_btn">
-        <i class="fa-solid fa-circle-plus"></i>
+        <i class="fa-solid fa-circle-plus add_list_icon"></i>
     </button>
 </form>
 
